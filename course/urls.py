dfee12aa7679ba9662b0c6c998bac87f903ae200
OiverAllen <1084946849@qq.com>
2019-09-23 22:06:33 +0800

    2-完成cours-01--2019/9.23

--- course/urls.py
+++ course/urls.py
@@ -9,7 +9,11 @@ urlpatterns = [
     url('^get_classify/',views.get_classify,name='get_classify'),
     url('^get_course/',views.get_course,name='get_course'),
     url('^get_course_number/',views.get_course_number,name='get_course_number'),
+    url('^search_course/',views.search_course,name='search_course'),
+    url('^search_course_number/',views.search_course_number,name='search_course_number'),
     url('^get_course_info/',views.get_course_info,name='get_course_info'),
+    url('^get_course_time/',views.get_course_time,name='get_course_time'),
+    url('^get_video_chapter/',views.get_video_chapter,name='get_video_chapter'),
     url('^get_chapter/',views.get_chapter,name='get_chapter'),
     url('^get_video/',views.get_video,name='get_video')
 ]
--- course/views.py
+++ course/views.py
@@ -10,20 +10,30 @@ def index(request):
 def get_difficulty(request):
     '''
     获取难度列表
-    :param request:
+    :param request:(全部)
     :return:  id name 【Json字符串】
     '''
-    pass
+    difficulty = Difficulty.objects.all()
+    if difficulty:
+        res_list = list(difficulty.values('id', 'name'))
+        return JsonResponse(res_list, json_dumps_params={'ensure_ascii': False}, safe=False)
+    else:
+        return JsonResponse({"Error": "获取数据错误"}, json_dumps_params={'ensure_ascii': False}, safe=False)
 
 
 
 def get_direction(request):
     '''
     获取方向列表
-    :param request:
+    :param request:(全部)
     :return:  id  name 【Json字符串】
     '''
-    pass
+    direction = Direction.objects.all()
+    if direction:
+        res_list = list(direction.values('id', 'name'))
+        return JsonResponse(res_list, json_dumps_params={'ensure_ascii': False}, safe=False)
+    else:
+        return JsonResponse({"Error":"获取数据错误"}, json_dumps_params={'ensure_ascii': False}, safe=False)
 
 
 # 获取 Classify【这是实例。。。】
@@ -33,8 +43,10 @@ def get_classify(request):
     :param request: direction_id // direction_name（两者为空则全部查询）
     :return: id   name  direction__name
     '''
-    direction_id = None # 11
-    direction_name = '' # '后端'
+    # direction_id = None # 11
+    # direction_name = '' # '后端'
+    direction_id = request.GET.get('direction_id')
+    direction_name = request.GET.get('direction_name')
     if direction_id:
         res = Classify.objects.filter(direction__id=direction_id)
     else:
@@ -49,7 +61,37 @@ def get_course(request):
     :param request: difficulty_name  direction_name  classify_name  page_index  page_items  sort_flag( 为0：按时间逆序，为1：选课人数，为2：收藏人数)
     :return:  id  name  introduce   course_icon   integral   publish_time【Json字符串】
     '''
-    pass
+    difficulty_name = request.GET.get('difficulty_name')
+    direction_name = request.GET.get('direction_name')
+    classify_name = request.GET.get('classify_name')
+    # page_index = request.GET.get('page_index')
+    # sort_flag = request.GET.get('sort_flag')
+    page_items = 5
+    page_index = 1
+
+    sort_flag = 1
+
+    if sort_flag == 0:
+        data = Course.objects.filter(difficulty__name__icontains=difficulty_name,classify__direction__name__icontains=direction_name,
+                                     classify__name__icontains=classify_name).order_by('-publish_time')[:5]
+    elif sort_flag == 1:
+        from django.db.models import Count
+        from user_course.models import SelectCourse
+        # users = SelectCourse.objects.values('course__name').annotate(select_num = Count('course_id')).order_by('-select_num')
+        # print(users.values())
+        # result = users.values('course__id','course__name','course__introduce',
+        #                    'course__course_icon','course__integral','course__publish_time')
+        # for res in result:
+        #     res['course__publish_time']=res['course__publish_time'].strftime('%Y-%m-%d %H:%M:%S')
+        # return JsonResponse(list(result), json_dumps_params={'ensure_ascii': False}, safe=False)
+    # return HttpResponse('ok')
+    result = data.values('id','name','introduce','course_icon','integral','publish_time')
+    for res in result:
+        res['publish_time']=res['publish_time'].strftime('%Y-%m-%d %H:%M:%S')
+    return JsonResponse(list(result), json_dumps_params={'ensure_ascii': False}, safe=False)
+
+
+
 
 
 def get_course_number(request):
@@ -58,7 +100,13 @@ def get_course_number(request):
     :param request: difficulty_name  direction_name  classify_name
     :return:  course_num 【Json字符串】
     '''
-    pass
+    difficulty_name = request.GET.get('difficulty_name')
+    direction_name = request.GET.get('direction_name')
+    classify_name = request.GET.get('classify_name')
+
+    data = Course.objects.filter(difficulty__name__icontains=difficulty_name,classify__direction__name__icontains=direction_name,
+                                 classify__name__icontains=classify_name).count()
+    return JsonResponse({"course_num": data}, json_dumps_params={'ensure_ascii': False}, safe=False)
 
 
 
@@ -68,7 +116,25 @@ def search_course(request):
     :param request: search_text（即 course_name 去匹配） sort_flag( 为0：按时间逆序，为1：选课人数，为2：收藏人数)】
     :return: id  name  introduce   course_icon   integral   publish_time【Json字符串】
     '''
-    pass
+    search_text = request.GET.get('search_text')
+    # sort_flag = request.GET.get('sort_flag')
+    sort_flag = 0
+
+    if search_text:
+        if sort_flag == 0:
+            data = Course.objects.filter(name__icontains=search_text).order_by('-publish_time')
+            result = list(data.values('id','name','introduce','course_icon','integral','publish_time'))
+            for res in result:
+                res['publish_time']=res['publish_time'].strftime('%Y-%m-%d %H:%M:%S')
+            return JsonResponse(result, json_dumps_params={'ensure_ascii': False}, safe=False)
+        elif sort_flag == 1:
+            pass
+        else:
+            pass
+    else:
+        return JsonResponse({"Error": "请输入搜素关键词"}, json_dumps_params={'ensure_ascii': False}, safe=False)
+
+
 
 
 
@@ -78,25 +144,78 @@ def search_course_number(request):
     :param request: search_text（即 course_name 去匹配）
     :return:   course_num 【Json字符串】
     '''
-    pass
+    search_text = request.GET.get('search_text')
+    if search_text:
+        data = Course.objects.filter(name__icontains=search_text).count()
+        return JsonResponse({"course_num": data}, json_dumps_params={'ensure_ascii': False}, safe=False)
+    else:
+        return JsonResponse({"Error": "请输入搜素关键词"}, json_dumps_params={'ensure_ascii': False}, safe=False)
+
+
 
 
 def get_course_info(request):
     '''
     获取课程详细信息
     :param request: course_id
-    :return:  publish_time  difficulty_name 【Json字符串】
+    :return: name  introduce   course_icon  notice  integral
+    difficulty_name  direction_name  classify_name study_people collection_people
     '''
-    pass
+    from django.db.models import Count
+    from user_course.models import SelectCourse, Collection
+    id = request.GET.get('course_id')
+
+    course_data = Course.objects.filter(id=id)
+    # 课程名，介绍，课程图片，课程须知，
+    result = list(course_data.values('name', 'introduce', 'course_icon', 'notice', 'integral', 'difficulty__name', 'classify__name', 'classify__direction__name'))
+    # 选课人数的判断
+    select_data = SelectCourse.objects.filter(course_id=id).values('course_id')\
+        .annotate(study_people=Count('course_id'))
+    if select_data:
+        select_res = list(select_data.values('study_people'))[0]['study_people']
+    else:
+        select_res = 0
+    result[0]['study_people'] = select_res
+
+    # 课程是否被收藏
+    collection_data = Collection.objects.filter(course_id=id).values('course_id')\
+        .annotate(collection_people=Count('course_id'))
+    if collection_data:
+        collection_res = list(collection_data.values('collection_people'))[0]['collection_people']
+    else:
+        collection_res = 0
+    result[0]['collection_people'] = collection_res
+    return JsonResponse(result, json_dumps_params={'ensure_ascii': False}, safe=False)
+
+
 
 
 def get_course_time(request):
     '''
     获取课程总时长（单独一个，不直接和get_video_info（）之类的函数交互）
     :param request:  course_id
-    :return: course_timr【Json字符串】
+    :return: course_time【Json字符串】
     '''
-    pass
+    from utils import time_to_stamp,stamp_to_time
+    time_stamp= 0
+    course_id = request.GET.get('course_id')
+
+    courseware_data = Courseware.objects.filter(chapter__course_id=course_id)
+    courseware_time = list(courseware_data.values('courseware_time'))
+    video_data = Video.objects.filter(chapter__course_id=course_id)
+    video_time = list(video_data.values('video_time'))
+
+    for i in range(len(courseware_time)):
+        courseware_time[i]['courseware_time']= time_to_stamp(courseware_time[i]['courseware_time'])
+        time_stamp += courseware_time[i]['courseware_time']
+
+    for i in range(len(video_time)):
+        video_time[i]['video_time'] = time_to_stamp(video_time[i]['video_time'])
+        time_stamp += video_time[i]['video_time']
+
+    course_time = stamp_to_time(time_stamp)
+    return JsonResponse({"course_time":course_time}, json_dumps_params={'ensure_ascii': False}, safe=False)
+
 
 
 
@@ -117,7 +236,20 @@ def get_video_chapter(request):
     :param request: course_id
     :return: 【Json字符串】。。。。这一部分还不确定，，
     '''
-    pass
+    course_id = request.GET.get('course_id')
+    course_video = Video.objects.filter(chapter__course_id=course_id)
+    res_list = list(course_video.values('id','name','chapter_id'))
+    chapter_info = get_chapter(request)
+    for i in range(len(chapter_info)):
+        chapter_info[i]['video'] = None
+        for j in range(len(res_list)):
+            if chapter_info[i]['id'] == res_list[j]['chapter_id']:
+                if chapter_info[i]['video']:
+                    chapter_info[i]['video'].append({"video_name": res_list[j]['name'], "video_id": res_list[j]['id']})
+                else:
+                    chapter_info[i]['video'] = [{"video_name": res_list[j]['name'], "video_id": res_list[j]['id']}]
+                # res_list.remove(res_list[j])
+    return JsonResponse(chapter_info, json_dumps_params={'ensure_ascii': False}, safe=False)
 
 
 
@@ -136,7 +268,12 @@ def get_chapter(request):
     :param request: course_id
     :return: id  name  introduce【Json字符串】
     '''
-    pass
+    course_id = request.GET.get('course_id')
+    chapter_data = Chapter.objects.filter(course_id=course_id)
+    res_list = list(chapter_data.values('id','name','introduce'))
+    return res_list
+    # return JsonResponse(res_list, json_dumps_params={'ensure_ascii': False}, safe=False)
+
 
 
 
